099a985dd3f096295d35c2083d7dc6fd889ab88f
Andrew.Hepp <git@ahepp.dev>
2022-12-20 01:18:56 -0800

    return 200 on post to settings

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,17 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn settings() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/settings", web::post().to(settings))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,23 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn settings_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "sp=500&kp=50&ki=1&kd=0&max_temp=1000";
+    let response = client
+        .post(&format!("{}/settings", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
     let port = listener.local_addr().unwrap().port();
