2a9802c9350e38af66701e5d37dafe9351fdeff8
Andrew.Hepp <git@ahepp.dev>
2022-12-20 02:51:52 -0800

    deserialize settings form data

--- src/routes/settings.rs
+++ src/routes/settings.rs
@@ -1,5 +1,14 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
 
-pub async fn settings() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    sp: i32,
+    kp: i32,
+    ki: i32,
+    kd: i32,
+    max_temp: i32,
+}
+
+pub async fn settings(form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
