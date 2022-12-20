c8fe22c0273473e7caa47a7138ce1eddae253122
Andrew.Hepp <git@ahepp.dev>
2022-12-20 09:41:55 -0800

    send settings via udp

--- src/routes/settings.rs
+++ src/routes/settings.rs
@@ -1,5 +1,8 @@
+use std::net::UdpSocket;
+
 use actix_web::{web, HttpResponse};
 
+#[repr(C)]
 #[derive(serde::Deserialize)]
 pub struct FormData {
     sp: i32,
@@ -9,6 +12,16 @@ pub struct FormData {
     max_temp: i32,
 }
 
-pub async fn settings(form: web::Form<FormData>) -> HttpResponse {
+fn cast_to_bytes(data: &FormData) -> &[u8] {
+    let ptr = data as *const FormData as *const u8;
+    let len = std::mem::size_of::<FormData>();
+    unsafe { std::slice::from_raw_parts(ptr, len) }
+}
+
+pub async fn settings(form: web::Form<FormData>, boilerd_port: web::Data<u16>) -> HttpResponse {
+    let sock = UdpSocket::bind("127.0.0.1:0").expect("Failed to bind.");
+    let dst = format!("127.0.0.1:{}", boilerd_port.as_ref());
+    let bytes = cast_to_bytes(&form);
+    sock.send_to(bytes, dst).expect("Failed to send.");
     HttpResponse::Ok().finish()
 }
--- src/startup.rs
+++ src/startup.rs
@@ -5,11 +5,12 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, boilerd_port: u16) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/settings", web::post().to(settings))
+            .app_data(web::Data::new(boilerd_port))
     })
     .listen(listener)?
     .run();
