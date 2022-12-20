7cb9ef2dcb971e502c9c049b82ba117437968a39
Andrew.Hepp <git@ahepp.dev>
2022-12-20 12:36:25 -0800

    bind to wildcard addr

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let configuration = apid::configuration::get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let configuration =
+        apid::configuration::get_configuration().expect("Failed to read configuration.");
+    let address = format!("0.0.0.0:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind.");
     apid::startup::run(listener, configuration.boilerd_port)?.await
 }
