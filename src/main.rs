c71ae56248affb049061b825a880e870a93d8f2c
Andrew.Hepp <git@ahepp.dev>
2022-12-20 00:36:00 -0800

    when testing, bind to random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use apid::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind.");
+    run(listener)?.await
 }
